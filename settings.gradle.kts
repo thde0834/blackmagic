ebcbaffad1a2ff7045b1d66e1edb6b40734d597a
thde0834 <thomas.deaner@gmail.com>
2024-10-12 16:50:06 +0900

    using kotlin dsl for settings.gradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,9 @@
+rootProject.name = "blackmagic"
+
 // A list of which subprojects to load as part of the same larger project.
 // You can remove Strings from the list and reload the Gradle project
 // if you want to temporarily disable a subproject.
-include 'lwjgl3', 'core', 'ios', 'android'
+include("lwjgl3")
+include("core")
+include("ios")
+include("android")
